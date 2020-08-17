4c58530f649d19b72aff68d542a5dd2b8aff139f
Ushan <Ushanjay>
2020-08-17 12:57:40 +0530

    IT19126784

---
+++ app/src/main/java/com/example/pathum/form.java
@@ -0,0 +1,20 @@
+package com.example.pathum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+    public void opensecond(View view){
+        Intent intent =new Intent(this,edit.class);
+        startActivity(intent);
+    }
+}
